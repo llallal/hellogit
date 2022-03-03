2fd1a9ac1bf4df74f512f33b74eec9fb48e685e9
llallal <90537073+llallal@users.noreply.github.com>
2022-03-03 16:07:38 +0800

    Update hello.java
    在线更新

--- git/src/main/java/hello.java
+++ git/src/main/java/hello.java
@@ -6,5 +6,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("开启分支");
+        System.out.println("在线更新");
     }
 }
